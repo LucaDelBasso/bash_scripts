6e0e0f37046d68f584cc2c0b5315d36df76dde1e
LucaDelBasso <lucadelbasso98@gmail.com>
2022-02-28 18:58:02 +0000

    hiding api key

--- sysinfo/json_parse.py
+++ sysinfo/json_parse.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 url = "https://numbersapi.p.rapidapi.com/random/trivia"
 
@@ -6,7 +7,7 @@ querystring = {"json":"true","fragment":"true","max":"1000","min":"0"}
 
 headers = {
     'x-rapidapi-host': "numbersapi.p.rapidapi.com",
-    'x-rapidapi-key': "2389ef68f4mshb47f2b8165e8bbfp107ca0jsn95349ab1c019"
+    'x-rapidapi-key': os.getenv("RAPID_API_KEY")
     }
 
 r = requests.request("GET", url, headers=headers, params=querystring).json()
